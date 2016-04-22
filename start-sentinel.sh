fa0fe65ecce601cd012605afe06f8b49634fcc66
Luis Elizondo <luis.elizondo@mx1.ibm.com>
2016-04-22 17:14:03 -0500

    Reduce failover timeout

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -14,7 +14,7 @@ echo "bind 0.0.0.0" > $SENTINEL_CONFIGURATION_FILE
 echo "port 6379" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel monitor $GROUP_NAME $MASTER_IP $MASTER_PORT $QUORUM" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel down-after-milliseconds $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
-echo "sentinel failover-timeout $GROUP_NAME 60000" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel failover-timeout $GROUP_NAME 5000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel parallel-syncs $GROUP_NAME 1" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel announce-ip ${ANNOUNCED_IP}" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel announce-port ${ANNOUNCED_PORT}" >> $SENTINEL_CONFIGURATION_FILE
