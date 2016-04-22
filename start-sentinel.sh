0f9eab29b3350254c1607618c8f893e1ae3f6bee
Luis Elizondo <luis.elizondo@mx1.ibm.com>
2016-04-22 16:15:03 -0500

    Enable announce-port and announce-ip in start-sentinel

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -5,6 +5,8 @@ set -e
 
 GROUP_NAME=${GROUP_NAME}
 QUORUM=${QUORUM}
+ANNOUNCED_IP=${ANNOUNCED_IP}
+ANNOUNCED_PORT=${ANNOUNCED_PORT}
 SENTINEL_CONFIGURATION_FILE=/etc/sentinel.conf
 
 # Host and port we will listen for requests on
@@ -14,5 +16,7 @@ echo "sentinel monitor $GROUP_NAME $MASTER_IP $MASTER_PORT $QUORUM" >> $SENTINEL
 echo "sentinel down-after-milliseconds $GROUP_NAME 60000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel failover-timeout $GROUP_NAME 180000" >> $SENTINEL_CONFIGURATION_FILE
 echo "sentinel parallel-syncs $GROUP_NAME 1" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel announce-ip ${ANNOUNCED_IP}" >> $SENTINEL_CONFIGURATION_FILE
+echo "sentinel announce-port ${ANNOUNCED_PORT}" >> $SENTINEL_CONFIGURATION_FILE
 
 /usr/local/bin/redis-server $SENTINEL_CONFIGURATION_FILE --sentinel
